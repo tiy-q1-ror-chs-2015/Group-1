4d0f05f68862b017620c53656c656edb22a1305b
Jesse Robinson <j3robinson1@gmail.com>
2015-02-19 16:39:12 -0500

    changed contact adress field

--- Goldteam_Backend/db/migrate/20150219200948_create_contacts.rb
+++ Goldteam_Backend/db/migrate/20150219200948_create_contacts.rb
@@ -2,7 +2,11 @@ class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :name
-      t.string :address
+      t.string :street
+      t.string :street_2
+      t.string :city
+      t.string :state
+      t.integer :zip
       t.string :email
       t.string :github
       t.string :twitter
