b0fe1e0a1ea2a00c00bc115b58ad03f1f43b944f
Brent O'Neill <brentoneill@gmail.com>
2015-02-19 16:24:10 -0500

    Fixed ruby error

--- Goldteam_Backend/app/controllers/home_controller.rb
+++ Goldteam_Backend/app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+  def index
+    respond_to do |format|
+      format.json {render json: @contacts.to_json}
+    end
+  end
 end
--- front/assets/js/main.js
+++ front/assets/js/main.js
@@ -4,5 +4,7 @@ var App = {};     //Global App variable for use w/ routes
 
 //Run the app once the DOM loads
 $(document).ready(function(){
-  
+  //ROUTER STUFF
+  App.router = new AppRouter();
+  Backbone.history.start();
 });
