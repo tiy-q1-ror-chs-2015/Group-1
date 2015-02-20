913895d79e3be33b5c294dff08f456a752d66faf
Brent O'Neill <brentoneill@gmail.com>
2015-02-19 20:28:10 -0500

    Add Content Up and Running - Front

--- front/assets/js/models.js
+++ front/assets/js/models.js
@@ -1,1 +1,13 @@
 //Contact - single contact model
+var Contact = Backbone.Model.extend({
+  urlRoot: 'http://localhost:9000/users/' + "1" + '/contacts',
+  defaults:function(){
+    name: 'John Doe'
+  },
+  initialize: function(){
+    console.log('contact has been initialized');
+  },
+  toJSON: function(){
+    return{contact: this.attributes};
+  }
+});
--- front/assets/js/routes.js
+++ front/assets/js/routes.js
@@ -1,3 +1,16 @@
 //ROUTES
 ///LogIN
 ////home (all contacts), add contact, edit contact
+var AppRouter = Backbone.Router.extend({
+  initialize: function() {
+  },
+  routes: {
+    '': 'home'
+  },
+  home: function(){
+    this.loadView(new AddContactView());
+  },
+  loadView:function(view){
+    this.view = view;
+  }
+});
